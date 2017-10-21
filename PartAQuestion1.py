d9de2f063b9d802273e5a8e6bf18179e2e3e971d
Sripradha-karkala <skarkala@wisc.edu>
2017-10-21 15:45:28 -0500

    Integrating with streaming

--- PartAQuestion1.py
+++ PartAQuestion1.py
@@ -1,7 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType
+import streaming_files
+from threading import thread
+
+STAGING_DIR='/home/ubuntu/assign2/partA/split-dataset'
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Usage: python PartAQuestion1.py <streaming_dir"
+        exit()
+    streaming_dir = sys.argv[1]
 
     spark = SparkSession \
             .builder \
@@ -16,7 +24,19 @@ if __name__ == '__main__':
 
     tweets = spark \
              .readStream \
-             .csv('/home/ubuntu/assign1/partA/streaming_dir', schema=input_schema)
+             .csv('hdfs:/home/ubuntu/assign2/partA/streaming_dir', schema=input_schema)
 
     # Split the tweets to get values
-    tweets.show()
+    interactions = tweets.groupBy('interaction').count()
+
+    query = interactions \
+            .writeStream \
+            .outputMode('complete') \
+            .format('console') \
+            .start()
+
+    query.awaitTermination()
+
+    #  Run this as a separate thread
+    streaming_thread = Thread(target=streaming_files.start_streaming, args=(STAGING_DIR, streaming_dir))
+    streaming_thread.start()
--- streaming_files.py
+++ streaming_files.py
@@ -5,23 +5,7 @@ import os
 
 LOCAL_PATH = '/home/ubuntu/assign2/spark-streaming/split-dataset'
 
-if __name__ == '__main__':
-    ''' The program periodically moves files from staging to the input directory
-        which gives the effect of streaming data
-
-        input_dir - Input dir path for storing data for streaming
-        staging_dir - Path to the staging dir where data is initially stored
-    '''
-    if len(sys.argv) != 3:
-        print "Usage: python streaming_files <input_dir> <staging_dir>"
-        exit();
-
-    streaming_dir = sys.argv[1]
-    staging_dir = sys.argv[2]
-
-
-
-
+def start_streaming(staging_dir, streaming_dir):
     # Initial setup
     #Source the required variables
     env_variables = subprocess.check_output('. ~/run.sh; env -0',
@@ -31,12 +15,15 @@ if __name__ == '__main__':
     os.environ.update(line.partition('=')[::2] for line in env_variables.split('\0'))
 
     # Copy files from local to staging dir
+    subprocess.call('hadoop fs -rmr ' + staging_dir + '/*',
+		    shell=True)
+
     subprocess.call('hadoop fs -copyFromLocal ' + LOCAL_PATH + '/* ' +staging_dir,
                     shell=True)
     # Clear any files already present in the streaming dir
     subprocess.call('hadoop fs -rmr ' + streaming_dir + '/*',
 		    shell=True)
-    
+
     # Get the count of files in staging dir
     count = subprocess.check_output(
             ['hdfs dfs -count ' + staging_dir+ ' | tail -1 | awk -F \' \' \'{print $2}\''],
@@ -51,7 +38,7 @@ if __name__ == '__main__':
 
         filename = filename.strip()
         # Move the file to input_dir
-	command = 'hdfs dfs -mv ' + filename + ' ' + streaming_dir
+        command = 'hdfs dfs -mv ' + filename + ' ' + streaming_dir
         subprocess.call(command, shell=True)
         time.sleep(5)
         # Get the count again
