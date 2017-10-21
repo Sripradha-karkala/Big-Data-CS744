44a0677857f7b428ae354fb96e8eed0af13a6626
Sripradha-karkala <skarkala@wisc.edu>
2017-10-21 23:20:58 +0000

    Working PartA1

--- PartAQuestion1.py
+++ PartAQuestion1.py
@@ -1,16 +1,26 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType
 from pyspark.sql.types import StringType
+from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import window
 import streaming_files
-from threading import thread
+from threading import Thread
+import sys
 
 STAGING_DIR='/home/ubuntu/assign2/partA/split-dataset'
+WINDOW_DURATION='60 minutes'
+UPDATE_DURATION='30 minutes'
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "Usage: python PartAQuestion1.py <streaming_dir"
         exit()
     streaming_dir = sys.argv[1]
+
+    streaming_files.initial_setup(STAGING_DIR, streaming_dir)    
+    # Start the streaming process
+    streaming_thread = Thread(target=streaming_files.start_streaming, args=(STAGING_DIR, streaming_dir))
+    streaming_thread.start()
 
     spark = SparkSession \
             .builder \
@@ -20,7 +30,7 @@ if __name__ == '__main__':
     input_schema = StructType() \
                     .add('userA', StringType()) \
                     .add('userB', StringType()) \
-                    .add('timestamp', StringType()) \
+                    .add('timestamp', TimestampType()) \
                     .add('interaction', StringType())
 
     tweets = spark \
@@ -28,16 +38,22 @@ if __name__ == '__main__':
              .csv('hdfs:/home/ubuntu/assign2/partA/streaming_dir', schema=input_schema)
 
     # Split the tweets to get values
-    interactions = tweets.groupBy('interaction').count()
+    interactions = tweets.groupBy(
+			window(tweets.timestamp, WINDOW_DURATION, UPDATE_DURATION),
+			tweets.interaction
+			).count()
+
 
     query = interactions \
             .writeStream \
             .outputMode('complete') \
             .format('console') \
+	    .option('truncate', 'false') \
+	    .option('numRows', 100000000) \
             .start()
 
     query.awaitTermination()
-
+    
     #  Run this as a separate thread
-    streaming_thread = Thread(target=streaming_files.start_streaming, args=(STAGING_DIR, streaming_dir))
-    streaming_thread.start()
+    # streaming_thread = Thread(target=streaming_files.start_streaming, args=(STAGING_DIR, streaming_dir))
+    # streaming_thread.start()
--- streaming_files.py
+++ streaming_files.py
@@ -4,7 +4,28 @@ import time
 import os
 
 LOCAL_PATH = '/home/ubuntu/assign2/spark-streaming/split-dataset'
+def initial_setup(staging_dir, streaming_dir):
+   # Initial setup
+    #Source the required variables
+    env_variables = subprocess.check_output('. ~/run.sh; env -0',
+                                            shell=True,
+                                            executable='/bin/bash')
+    os.environ.clear()
+    os.environ.update(line.partition('=')[::2] for line in env_variables.split('\0'))
 
+    # Copy files from local to staging dir
+    
+    subprocess.call('hadoop fs -rmr ' + staging_dir + '/*',
+                    shell=True)
+
+    print 'Copying files from local to staging environment'
+
+    subprocess.call('hadoop fs -copyFromLocal ' + LOCAL_PATH + '/* ' +staging_dir,
+                    shell=True)
+    # Clear any files already present in the streaming dir
+    subprocess.call('hadoop fs -rmr ' + streaming_dir + '/*',
+                    shell=True)
+ 
 def start_streaming(staging_dir, streaming_dir):
     # Initial setup
     #Source the required variables
@@ -12,17 +33,9 @@ def start_streaming(staging_dir, streaming_dir):
                                             shell=True,
                                             executable='/bin/bash')
 
+    # os.environ.clear()
     os.environ.update(line.partition('=')[::2] for line in env_variables.split('\0'))
 
-    # Copy files from local to staging dir
-    subprocess.call('hadoop fs -rmr ' + staging_dir + '/*',
-		    shell=True)
-
-    subprocess.call('hadoop fs -copyFromLocal ' + LOCAL_PATH + '/* ' +staging_dir,
-                    shell=True)
-    # Clear any files already present in the streaming dir
-    subprocess.call('hadoop fs -rmr ' + streaming_dir + '/*',
-		    shell=True)
 
     # Get the count of files in staging dir
     count = subprocess.check_output(
