1affffc1eff84f9ac5c05f2f2287c25b7f40eca6
Sripradha-karkala <skarkala@wisc.edu>
2017-10-21 11:59:23 -0500

    Script for setting up the streaming dir

---
+++ streaming_files.py
@@ -0,0 +1,47 @@
+import sys
+import subprocess
+import time
+import os
+
+if __name__ == '__main__':
+    ''' The program periodically moves files from staging to the input directory
+        which gives the effect of streaming data
+
+        input_dir - Input dir path for storing data for streaming
+        staging_dir - Path to the staging dir where data is initially stored
+    '''
+    if len(sys.argv) != 3:
+        print "Usage: python streaming_files <input_dir> <staging_dir>"
+        exit();
+
+    input_dir = sys.argv[0];
+    staging_dir = sys.argv[1];
+
+    #Source the required variables
+    env_variables = subprocess.check_output('. ~/run.sh',
+                                            shell=True,
+                                            executable='/bin/bash')
+    # Update the env for this python process
+    os.environ.update(line.partition('=')[::2] for line in env_variables.split('\0'))
+    # Get the count of files in staging dir
+    count = subprocess.check_output(
+            ['hdfs dfs -count ' + staging_dir+ ' | tail -1 | awk -F \' \' \'{print $2}\''],
+            shell=True)
+    count = count.strip()
+
+    while int(count) > 0:
+        # Get the first file in the staging dir
+        filename = subprocess.check_output(
+        ['hdfs dfs -ls ' + staging_dir + ' | head -2 | awk -F\' \' \'{print $8}\''],
+        shell=True)
+
+        filename = filename.strip()
+
+        # Move the file to input_dir
+        subprocess.call('hdfs dfs -mv ' + staging_dir + '/' + filename + ' ' + input_dir)
+        time.sleep(5)
+        # Get the count again
+        count = subprocess.check_output(subprocess.check_output(
+                ['hdfs dfs -count ' + staging_dir + ' | tail -1 | awk -F \' \' \'{print $2}\''],
+                shell=True))
+        count = count.strip()
